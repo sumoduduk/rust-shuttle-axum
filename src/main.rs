544970a9c4304e51ed435a1d5e0a6938c5214a90
sumoduduk <sumoduduk13@gmail.com>
2024-01-05 14:38:00 -0500

    register nested route contact form

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
 mod error;
+mod porto;
 mod remote_work;
 mod utils;
 
-use axum::{http::Method, routing::get, Router};
+use axum::{extract::FromRef, http::Method, routing::get, Router};
+use porto::porto_route;
 use remote_work::get_job;
 use shuttle_secrets::SecretStore;
 use tower_http::cors::{Any, CorsLayer};
 
 #[derive(Clone)]
+struct PortoState {
+    porto_id: String,
+    porto_bearer: String,
+    db_id: String,
+    porto_url: String,
+}
+
+#[derive(Clone)]
 pub struct AppState {
     uri: String,
+    porto_state: PortoState,
 }
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> shuttle_axum::ShuttleAxum {
+    let porto_state = PortoState {
+        porto_id: secret_store.get("NOTION_PORTO_ID").unwrap(),
+        porto_bearer: secret_store.get("BEARER").unwrap(),
+        db_id: secret_store.get("NOTION_DB_ID").unwrap(),
+        porto_url: secret_store.get("NOTION_URL").unwrap(),
+    };
+
     let secret_uri = secret_store.get("URI_ENDPOINT").unwrap();
 
-    let app_state = AppState { uri: secret_uri };
+    let app_state = AppState {
+        uri: secret_uri,
+        porto_state,
+    };
 
     let cors = CorsLayer::new()
         .allow_methods([Method::GET])
@@ -26,6 +47,7 @@ async fn main(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> shuttle_
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/search_job", get(get_job))
+        .nest("/porto", porto_route)
         .layer(cors)
         .with_state(app_state);
 
